1c47b3f17cfdb8685af3bf0a21a13874926203b6
Joy Phoebe <joyapisi@gmail.com>
2023-08-08 00:13:56 +0300

    Style forms for log in and sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,9 +60,9 @@ class UsersController < ApplicationController
   
     private
       # Use callbacks to share common setup or constraints between actions.
-      # def set_user
-      #   @user = user.find(params[:id])
-      # end
+      def set_user
+        @user = User.find(params[:id])
+      end
   
       # Only allow a list of trusted parameters through.
       def user_params
