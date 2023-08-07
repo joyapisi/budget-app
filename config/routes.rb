308afc7769d52a5826a4f6d25ea1e2ae5ae8f4da
Joy Phoebe <joyapisi@gmail.com>
2023-08-08 00:56:39 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,17 @@ Rails.application.routes.draw do
   resources :users
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-
+  resources :users, only: [:index, :show, :new, :create] do 
+    resources :clothes, only: [:index, :show, :new, :create, :destroy] do
+      resources :all_spendings, only: [:index, :show, :new, :create, :destroy]
+    end
+    resources :groups, only: [:index, :show, :new, :create, :destroy] do
+      resources :all_spendings, only: [:index, :show, :new, :create, :destroy] do
+    end
+    resources :spending_list, only: [:index, :show, :new, :create]
+  end
+end
+post 'spending_list/group_id=:group_id', to: 'groups#generate_spending_list', as: 'generate_spending_list'
+get 'spending_list/group_id=:group_id', to: 'groups#generate', as: "spending_list"
+resources :public_groups
 end
