6d9dca17dea7c0a59b57b58541502a4ca6566026
Joy Phoebe <joyapisi@gmail.com>
2023-08-09 13:10:35 +0300

    Pluralize names in has_many associations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_many :cloth
+  has_many :clothes
 
-  validates :name, presence: true
   validates :icon, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :cloth
-  has_many :group
+  has_many :clothes
+  has_many :groups
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
