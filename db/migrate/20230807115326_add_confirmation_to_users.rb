42241e111512863fce5c0232ff967be10006fc0e
Joy Phoebe <joyapisi@gmail.com>
2023-08-11 13:05:19 +0300

    Fix migration errors

--- db/migrate/20230807115326_add_confirmation_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddConfirmationToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :unconfirmed_email, :string
-    add_index :users, :confirmation_token, unique: true
-  end
-end
--- db/migrate/20230810064527_add_total_amount_to_groups.rb
+++
@@ -1,5 +0,0 @@
-class AddTotalAmountToGroups < ActiveRecord::Migration[7.0]
-  def change
-    add_column :groups, :total_amount, :float, default: 0
-  end
-end
