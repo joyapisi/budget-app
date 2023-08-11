138902197cf90abc768c487e3b2b15029f4477d9
Joy Phoebe <joyapisi@gmail.com>
2023-08-11 15:05:04 +0300

    Fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  root to: "groups#index"
-  
+  root to: 'groups#index'
+
   devise_for :users
   resources :users
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-    resources :groups, only: [:index, :show, :new, :create, :destroy] do
-      resources :clothes, only: [:index, :show, :new, :create, :destroy]
-    end  
-    get '/landing_page', to: 'landing_page#index'
+  resources :groups, only: %i[index show new create destroy] do
+    resources :clothes, only: %i[index show new create destroy]
+  end
+  get '/landing_page', to: 'landing_page#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_10_141321) do
+ActiveRecord::Schema[7.0].define(version: 20_230_810_141_321) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "clothes", force: :cascade do |t|
-    t.string "name"
-    t.integer "user_id"
-    t.integer "group_id"
-    t.integer "author_id"
-    t.decimal "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'clothes', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'user_id'
+    t.integer 'group_id'
+    t.integer 'author_id'
+    t.decimal 'amount'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "groups", force: :cascade do |t|
-    t.string "name"
-    t.string "icon"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.float "total_amount", default: 0.0
+  create_table 'groups', force: :cascade do |t|
+    t.string 'name'
+    t.string 'icon'
+    t.integer 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.float 'total_amount', default: 0.0
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.string 'unconfirmed_email'
+    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-
 end
